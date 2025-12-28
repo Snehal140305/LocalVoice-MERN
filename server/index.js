c2bc9fa9ce5e924322399fbb74c589d940c5612c
Snehal140305 <snehalkendre05@gmail.com>
2025-12-28 15:16:30 +0530

    Updated CORS for Netlify frontend

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "https://your-netlify-site.netlify.app"
+    "https://localvoice-backend.onrender.com"
   ],
   credentials: true
 }));
